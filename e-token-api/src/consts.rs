afbdd40df87a856edc4286deb9923221bb36fbee
Gabriele Picco <piccogabriele@gmail.com>
2026-03-30 22:25:16 +0200

    feat: add simpler transfer from base to delegated account (#38)
    * feat: add simpler transfer from base to delegated account
    
    * chore: refactor
    
    * chore: refactor
    
    * chore: refactor
    
    * chore: refactor
    
    * fix: workflow
    
    * chore: upgrade SDK in API
    
    * chore: revert workflow

--- e-token-api/src/consts.rs
+++ e-token-api/src/consts.rs
@@ -2,3 +2,8 @@
 ///
 /// 0.0005 SOL = 500_000 lamports.
 pub const SPONSORED_SHUTTLE_DELEGATION_SETUP_LAMPORTS: u64 = 500_000;
+
+/// Flat lamport fee charged by sponsored lamports transfer flows.
+///
+/// 0.0003 SOL = 300_000 lamports.
+pub const SPONSORED_LAMPORTS_TRANSFER_SETUP_LAMPORTS: u64 = 300_000;
